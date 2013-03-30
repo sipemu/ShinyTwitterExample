373c3c7067e84fcf61914545651e5ecc425362da
Simon Müller <info@muon-stat.com>
2013-03-30 19:41:43 +0100

    App finished

--- server.R
+++ server.R
@@ -1,33 +1,40 @@
 library(shiny)
 library(ggplot2)
+library(ROAuth)
 
 source("tools/twitteR.R")
 
 shinyServer(function(input, output) {
   
-  output$plot <- renderImage({
-
-    if (input$saveButton == 0)
-      return()
-    isolate({
-       df <- hour_dist(string = input$TwitterInput, 
-                       n      = 1200, 
-                       days   = 6, 
-                       lang   = "de", 
-                       plot   = FALSE)
-       
-       png("out.png", width=600, height=400)
-       ggplot(df) + geom_histogram(aes(x=hourly.count), binwidth=1) + 
-           facet_wrap(~date) + xlab("") + ylab("") + xlim(c(0,24))
-       dev.off()
-       list(src = "out.png",
-            alt = "This is alternate text")
-    })
+  twit <- reactive({
+    hour_dist(string = input$TwitterInput, 
+              n      = input$AmountInput, 
+              days   = input$DaysInput, 
+              lang   = "de", 
+              plot   = FALSE)
+  })
+ 
+  
+  output$plot <- renderPlot({
+     
+       print(ggplot(twit()) + geom_histogram(aes(x=hourly.count), binwidth=1) + 
+           facet_wrap(~date) + xlab("") + ylab("") + xlim(c(0,24)))    
   })
   
  
-  
   output$values <- renderTable({
-    tabelle()
-  }, deleteFile = TRUE)
+      tab <- sort(table(twit()$user), decreasing=T)[1:10]
+      df <- data.frame(V1=names(tab), V2=tab)
+      rownames(df) <- NULL
+      names(df) <- c("Name des Twitterers", "Anzahl Tweets")
+      df
+  })
+  
+  
+  output$downloadData <- downloadHandler(
+      filename <- function() { paste('results.csv', sep='') },
+      content <- function(file=filename) {
+        write.csv(twit(), file)
+      }
+  )
 })
--- ui.R
+++ ui.R
@@ -3,12 +3,26 @@ library(shinyIncubator)
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Eingabe Test"),
+  headerPanel("Rudimentärer Twitter Report"),
   
-  sidebarPanel(textInput("TwitterInput", "Twitter-Suchbegriff:", ""),
-               br(),
-               actionButton("saveButton", "Einfügen"), uiOutput("reload")),
+  sidebarPanel(h4('Einleitung'),
+               helpText(
+                 p("Die Grafik präsentiert die Anzahl der Tweets je Stunde und Tag zu einem Suchbegriff. Dabei 
+    ist lediglich möglich die Daten bis zu 7 Tage zurück zu erhalten unundebugie Anzahl 
+    der abrufbaren Tweets sind je Tag auf 1.500 beschränkt."), 
+                        p("Shiny ist experimentell. Nutzung auf eigene Gefahr."),
+                        p("Die Suche kann je nach Parameter lange dauern.")),
+    br(),
+    h4('Eingabe'), textInput("TwitterInput", "Twitter-Suchbegriff:", "Stuttgart"),
+               numericInput("DaysInput", "Wieviel Tage zurück (max. 7):", value=0, min=0, max=7, step=1),
+               numericInput("AmountInput", "Wieviele Tweets (max. 1500):", value=1, min=1, max=1500, step=1), br(),br(),
+               div(class="row-fluid", div(class="span6", submitButton(text="Suchen")), 
+                   downloadButton('downloadData', 'Download der Suche  als csv'))),
   
-  mainPanel(imageOutput("plot"))
-  
+  mainPanel(tabsetPanel(
+    tabPanel("Grafik", h4("Ausgabe"), imageOutput("plot"), 
+             h4("Top-Ten-Twitterer"), tableOutput("values")),
+    tabPanel("Über", wellPanel(includeMarkdown("ueber.Rmd"))) 
+                        )      
+            )
 ))
